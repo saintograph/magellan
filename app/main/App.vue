fdce22db6887c9db988866199edd99eed2a487a2
winfredselwyn <winfredselwyn@gmail.com>
2017-12-20 22:48:54 +0800

    Boilerplate code generated successfully.

---
+++ app/main/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <header class="l-app__titlebar titlebar"></header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Magellan'
+}
+</script>
+
+<style>
+</style>
